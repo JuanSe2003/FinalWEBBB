133227e2b2870e545e4510f1fb9ea672e6ef5901
Sebastian Pedraza <js.pedrazap1@uniandes.edu.co>
2024-05-24 21:36:36 -0500

    Cambios de test

--- src/profesor/profesor.service.spec.ts
+++ src/profesor/profesor.service.spec.ts
@@ -1,19 +1,48 @@
 /* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProfesorEntity } from './profesor.entity';
 import { ProfesorService } from './profesor.service';
-
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { faker } from '@faker-js/faker';
 describe('ProfesorService', () => {
   let service: ProfesorService;
+  let repositoryProfesores: Repository<ProfesorEntity>;
+  let profesoresList: ProfesorEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [ProfesorService],
     }).compile();
 
     service = module.get<ProfesorService>(ProfesorService);
+    repositoryProfesores = module.get<Repository<ProfesorEntity>>(getRepositoryToken(ProfesorEntity));
+    await seedDatabase();
   });
 
+
+  const seedDatabase = async () => {
+    repositoryProfesores.clear();
+    profesoresList = [];
+    for (let i=0; i<5; i++) {
+      const profesor :ProfesorEntity= await repositoryProfesores.save({
+        numeroCedula: faker.number.int(),
+        numeroextension: faker.number.int(),
+        nombre: faker.person.firstName(),
+        grupoInvestigacion: "TICSW",
+        propuestas:[],
+        
+      });
+      profesoresList.push(profesor);
+    }
+  };
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+}
+);
+
+
+
